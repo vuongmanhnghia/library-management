66104262da09790fc1ddc73c5ac460ebe6fe88dd
lucas-tran05 <trancuong13112005@gmail.com>
2025-01-16 19:27:32 +0700

    Fixe (redux): upgrade redux router

--- libary/src/redux/userSlice.jsx
+++ libary/src/redux/userSlice.jsx
@@ -5,24 +5,11 @@ export const userSlice = createSlice({
     name: "user",
     initialState: {
         user: null,
-        email: "Undefined",
-        phone:  "Undefined",
-        gender: "Undefined",
-        avatar: undefined,
-        birth: null,
-        country: 'Vietnam',
-        role: '0',
-        name: "Undefined",
     },
     reducers: {
         update: (state, action) => {
             // Cập nhật thông tin người dùng từ action payload
-            state.user = action.payload.user;
-            state.email = action.payload.email;
-            state.phone = action.payload.phone;
-            state.avatar = action.payload.avatar;
-            state.role = action.payload.role;
-            state.name = action.payload.name;
+            state.user = action.payload.user;   
         }
     },
 });
